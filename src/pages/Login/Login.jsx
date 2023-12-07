6268d1ee9f45685b2146cb1fd443aae27899b3f3
louis-marceron <louis.marceron@protonmail.com>
2023-12-07 21:26:56 +0100

    switch randomly the css after each reload

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,25 +1,45 @@
-import { useRef, useEffect } from 'react'
-
-import './css/style.css' 
-import myFunction from './js/myScript.js' 
+import { useRef, useEffect } from 'react';
 
 const Login = () => {
-    const ref = useRef(null)
+    const ref = useRef(null);
 
     useEffect(() => {
-        myFunction() // Exécuter votre fonction JS vanilla
 
-        // Exemple d'interaction avec le DOM
+        // Randomly select a CSS file
+        const cssFiles = ['cute', 'horror'];
+        const randomCssFile = cssFiles[Math.floor(Math.random() * cssFiles.length)];
+
+        // Dynamically import the CSS file
+        import(`./css/${randomCssFile}.css`)
+            .then(() => {
+                console.log(`Dynamically loaded ${randomCssFile}.css`);
+            })
+            .catch(err => console.error('Error loading CSS file:', err));
+
         if (ref.current) {
-            ref.current.innerHTML = '<p>Ceci est du contenu HTML inséré</p>'
+            // ref.current.innerHTML = '<p>Hahahahha</p>';
         }
-    }, [])
+    }, []);
 
     return (
         <div ref={ref}>
-            {/* Votre contenu JSX ici */}
+        <div class="login-container">
+        <h1>Login</h1>
+        <form action="#" method="post">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username"/>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password"/>
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    </div>
         </div>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
+
