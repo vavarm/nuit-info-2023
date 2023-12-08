b3e894c569285c34fe76b7da63f6b4575d320a2f
TAIP25 <bouleon25@gmail.com>
2023-12-08 06:13:37 +0100

    V3.2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 import '../CSS/Header.css'; 
 
 function Header() {
   return (
     <header>
-        <Grid>
+        <Grid container sx={{ justifyContent: 'center', gap:'25px' }}>
           <Link to="/login">
             <Button variant="contained" id="login" color="success"> Login </Button>
           </Link>
+
           <Link to="/quiz">
             <Button variant="contained" id="quiz" color="success"> Quiz </Button>
           </Link>
